4d161c41487c5db6084f11185abadb951a217fa7
AlexanderVak <mamorunokitsune@gmail.com>
2021-07-20 18:08:01 +0300

    add toggle, postpone, isOverdue,  increment methods

--- task.js
+++ task.js
@@ -1,21 +1,62 @@
 class Task{
+        
+    set Description(newVal){
+        this.desc = newVal
+    }
+
+    set DueDate(newVal){
+        this.dueDate = newVal
+    }
+
+    static incrementId(){
+        if(!this.latestId){
+            this.latestId = 1
+        } 
+        else this.latestId++
+        return this.latestId
+    }
     
+
     constructor(title, desc, dueDate){
-        this.id = 1
+        this.id =  Task.incrementId()
         this.title = title
         this.desc = desc
         this.dueDate = dueDate
-        this.done = true
+        this.done = false
+    }
+
+
+    toggle() {
+        this.done = !this.done 
     }
     
+    postpone (timeSpan){
+        let result = this.dueDate.setDate(this.dueDate.getDate() + timeSpan)
+        return result
+    }
+
+    isOverdue(){
+        return new Date() > this.dueDate
+    }
     
     toString(){        
-        let parsedDate = this.dueDate.toLocaleString("default", {month:'long', day: 'numeric'})
-        return `${this.id}. [${this.done ? 'x': ''}] ${this.title} (${parsedDate})\n${this.desc}`
+        let parsedDate = this.dueDate.toLocaleString("default", {month:'short', day: 'numeric'})
+        return `${this.id}. [${this.done ? 'x': ' '}] ${this.title} (${parsedDate})\n${this.desc} ${this.isOverdue() ? '\nYour task is overdue' : ''}`
     }
 }
 
-const date = new Date(2021, 8, 30)
-const task = new Task('Title', 'Descriptiopn', date);
+const date1 = new Date(2021, 3, 30)
+const date2 = new Date(2021, 4, 25)
+const date3 = new Date(2021, 5, 30)
 
-console.log(task.toString());
+const task1 = new Task('Title','Description', date1);
+const task2 = new Task('Title', 'Descriptiopn', date2);
+const task3 = new Task('Title', 'Descriptiopn', date3);
+
+console.log('---before toggle---\n',task1.toString());
+task2.toggle()
+task2.postpone(1)
+console.log('---first toggle---\n',task2.toString());
+task3.toggle()
+task3.toggle()
+console.log('---second toggle---\n',task3.toString());
